53331314bdf53bdd701639919518409ed5452568
Wasanjith Kodikara <149646160+wasanjith@users.noreply.github.com>
2025-09-08 08:35:48 +0530

    Add experience model and fialment resource

---
+++ app/Filament/Resources/ExperienceResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ExperienceResource\Pages;
+use App\Filament\Resources\ExperienceResource\RelationManagers;
+use App\Models\Experience;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ExperienceResource extends Resource
+{
+    protected static ?string $model = Experience::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required(),
+                Forms\Components\FileUpload::make('image')
+                    ->disk('public')
+                    ->visibility('public')
+                    ->image()
+                    ->required(),
+                Forms\Components\Textarea::make('inclusions')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('description')->limit(50)->sortable()->searchable(),
+                Tables\Columns\ImageColumn::make('image')
+                    ->disk('public')
+                    ->label('Image')
+                    ->rounded(),
+                Tables\Columns\TextColumn::make('inclusions')->limit(50)->sortable()->searchable(),
+                
+            ])
+            ->filters([
+                
+                // Tables\Filters\TrashedFilter::make(),
+
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ForceDeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListExperiences::route('/'),
+            'create' => Pages\CreateExperience::route('/create'),
+            'edit' => Pages\EditExperience::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Experience.php
+++ app/Models/Experience.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Experience extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'inclusions',
+    ];
+
+    public function getImageUrlAttribute()
+    {
+        if ($this->image) {
+            return Storage::disk('public')->url($this->image);
+        }
+        return null;
+    }
 }
